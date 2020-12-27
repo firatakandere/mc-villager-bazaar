f591c8dbcd403f041d0c6d9d2cec0f9612568e80
Firat Akandere <f.akandere@gmail.com>
2020-12-27 20:02:16 +0200

    Remove unused assignment

--- src/main/java/github/fakandere/villagerBazaar/utils/AnvilGUIHelper.java
+++ src/main/java/github/fakandere/villagerBazaar/utils/AnvilGUIHelper.java
@@ -6,15 +6,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.function.Consumer;
 
 public class AnvilGUIHelper {
-
     static JavaPlugin plugin;
-
     public static void setUp(JavaPlugin plg) {
         plugin = plg;
     }
-
     public static void prompt(Player p, String title, String text, Consumer<String> onComplete) {
-        AnvilGUI gui = new AnvilGUI.Builder()
+        new AnvilGUI.Builder()
                 .onComplete((player, tx) -> {                             //called when the inventory output slot is clicked
                     onComplete.accept(tx);
                     return AnvilGUI.Response.close();
