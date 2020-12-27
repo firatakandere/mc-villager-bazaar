9185bf77ed9167d55a05639dc87fcf5528786462
Firat Akandere <f.akandere@gmail.com>
2020-12-27 19:39:03 +0200

    Abstract create command

--- src/main/java/github/fakandere/villagerBazaar/commands/CreateAdminCommand.java
+++ src/main/java/github/fakandere/villagerBazaar/commands/CreateAdminCommand.java
@@ -1,20 +1,15 @@
 package github.fakandere.villagerBazaar.commands;
 
-import github.fakandere.villagerBazaar.utils.AnvilGUIHelper;
 import github.fakandere.villagerBazaar.utils.IBazaarManager;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
 
 import javax.inject.Inject;
 import java.rmi.UnexpectedException;
 import java.util.UUID;
 
-public class CreateAdminCommand implements CommandExecutor {
+public class CreateAdminCommand extends AbstractCreateCommand {
     IBazaarManager bazaarManager;
 
     @Inject
@@ -33,24 +28,5 @@ public class CreateAdminCommand implements CommandExecutor {
         }
         p.sendMessage(villagerID.toString());
         return true;
-    }
-
-    private UUID createVillager(Player p) {
-        Location targetLocation = p.getTargetBlock(null, 10).getLocation();
-        targetLocation.add(0.5, 1, 0.5);
-        //@todo: Is this block appropriate is it lava or water or anything sketchy ?
-        //throw VillagerShopInvalidPlacementException
-        Villager v = (Villager) p.getWorld().spawnEntity(targetLocation, EntityType.VILLAGER);
-        //Set properties
-        v.setCustomName("Villager of " + p.getName());
-        v.setInvulnerable(true);
-        v.setAI(false);
-        v.setVillagerLevel(5);
-        v.setVillagerType(Villager.Type.PLAINS);
-        v.setProfession(Villager.Profession.NONE);
-
-        AnvilGUIHelper.prompt(p, "What is your shop's name", "Shop?", (text) -> v.setCustomName(text.replaceAll("[^a-zA-Z0-9\\s]", "")));
-
-        return v.getUniqueId();
     }
 }
